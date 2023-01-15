ff269dd7c789943201c5c4d84f7f487a0cb27af8
Juan Pablo Elias <eliasjuanpablo1@gmail.com>
2023-01-15 15:41:48 -0300

    capture user input, validate keyboard language

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { cyrillicToLatinMap } from "./utils";
 import "./App.css";
 
 function App() {
+  const [languageError, setLanguageError] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  function handleKeyDown(e: KeyboardEvent) {
+    const isRelevantKey = e.key !== "Backspace" && !e.altKey && !e.metaKey;
+    if (!isRelevantKey) return;
+
+    const latinKey = cyrillicToLatinMap[e.key];
+    if (!latinKey) {
+      // User is not writing in russian
+      e.preventDefault();
+      setLanguageError(true);
+      return;
+    }
+
+    setLanguageError(false);
+  }
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -11,6 +37,8 @@ function App() {
             background: "rgba(255, 255, 255, 0.3)",
             padding: "2rem 8rem",
             borderRadius: "10px",
+            borderBottom: "2px solid grey",
+            width: "300px",
           }}
         >
           <h1 style={{ fontSize: "4rem" }}>Сирило</h1>
@@ -18,7 +46,11 @@ function App() {
             <div className="">Your word is:</div>
             <div className="bold text-l">сирило</div>
           </div>
-          <input type="text" />
+          {languageError && (
+            <div className="error">
+              Whoops! it seems you are not writing cyrillic...
+            </div>
+          )}
         </div>
       </div>
     </div>
